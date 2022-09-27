34c33e6727a8480a328902215e5a5b1b1465ca07
alt-Pranav <74000887+alt-Pranav@users.noreply.github.com>
2022-09-27 17:39:55 +0530

    #1, streamlit app works

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import pytube
+from moviepy.editor import VideoFileClip
+import os
+
+def download_video(url):
+    video = pytube.YouTube(url)
+    stream = video.streams.get_by_itag(18)
+    stream.download()
+    return stream.default_filename
+
+def convert_to_mp3(filename):
+    clip = VideoFileClip(filename)
+    audioName = filename[:-4] + ".mp3"
+    clip.audio.write_audiofile(audioName)
+    clip.close()
+    return audioName
+
+def main(link):
+    try:
+        filename = download_video(link)
+        print("Downloaded video as " + filename)
+    except:
+        print("Not a valid link..")
+    try:
+        convert_to_mp3(filename)
+        print("Converted video to mp3")
+    except:
+        print("Error converting video to mp3")
+
+
+# Headings for Web App
+st.title("YouTube to MP3 converter")
+
+# textbox used for getting video link
+st.subheader("Enter the video link below")
+vidLink = st.text_input("Enter Link: ")
+if vidLink:
+    
+    # display converted mp3 file for download
+    st.subheader("Result")
+
+    filename = download_video(vidLink)
+    st.text("Downloaded video as " + filename)
+
+    audioFile = convert_to_mp3(filename)
+    st.text("Converted video to mp3")
+
+    os.remove(filename)
+    
+    # Defaults to 'application/octet-stream'
+    with open(audioFile, "rb") as file:
+        btn = st.download_button(
+                label="Download",
+                data=file,
+                file_name=audioFile,
+            )
+
+    os.remove(audioFile)
+    # this works! - st.text("Audio file removed from system")
+
+
