eee53f78f80cae621948e1ed4d5224c6db59dd22
alt-Pranav <74000887+alt-Pranav@users.noreply.github.com>
2023-03-02 17:19:56 +0530

    added toggle to change from mp3 to wav

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ def download_video(url):
 
 def convert_to_mp3(filename):
     clip = VideoFileClip(filename)
-    audioName = filename[:-4] + ".mp3"
+    #audioName = filename[:-4] + ".mp3" 
+    audioName = filename[:-4] + f".{selected_ext}"
     clip.audio.write_audiofile(audioName)
     clip.close()
     return audioName
 
+st.set_page_config(page_title="Video Converter")
+
 # Headings for Web App
-st.title("YouTube to MP3 converter")
+st.title("YouTube converter")
+
+extensions = dict(mp3="mp3", wav="wav")
+selected_ext = st.selectbox("Choose the file extension you want", extensions)
 
 # textbox used for getting video link
 st.subheader("Enter the video link below")
@@ -32,7 +38,7 @@ if vidLink:
     st.text("Downloaded video as " + filename)
 
     audioFile = convert_to_mp3(filename)
-    st.text("Converted video to mp3")
+    st.text(f"Converted video to {selected_ext}")
 
     os.remove(filename)
     
@@ -40,7 +46,7 @@ if vidLink:
     # Defaults to 'application/octet-stream'
     with open(audioFile, "rb") as file:
 
-        st.audio(data=file, format='audio/mp3', start_time=0)
+        st.audio(data=file, format=f'audio/{selected_ext}', start_time=0)
 
         vidName = ""
         vidName = st.text_input("(Optional) Save as: (default= {0}) ".format(audioFile[:-4]))
@@ -52,7 +58,7 @@ if vidLink:
                 label="Download",
                 data=file,
                 #file_name= audioFile ,
-                file_name= vidName+".mp3",
+                file_name= vidName+f".{selected_ext}",
             )
       
 
